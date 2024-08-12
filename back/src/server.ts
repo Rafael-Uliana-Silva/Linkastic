fd718c0298236d6e4724a34713be1bed338c8731
Rafael <rafaeluliana2017@gmail.com>
2024-08-12 18:51:24 -0300

    Commit inicial

---
+++ back/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from "cors"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI || '')
+.then(() => console.log("Conectado ao MongoDB"))
+.catch(err => console.log(`Houve um erro de conexão: ${err}`));
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server iniciado na url: http://localhost:${PORT}`);
+});
